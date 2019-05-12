e8a33dbd37919a83513e73bc57bcbf86c968b9a5
Viki Berkle <vberkle@gmail.com>
2019-05-12 23:15:20 +0700

    format

--- app/currency_converter/currency.py
+++ app/currency_converter/currency.py
@@ -8,13 +8,13 @@ def find_element(root, code_valute):
         if valute.find('CharCode').text.upper() == code_valute.upper():
             currency_data = {'name_of_currency': valute.find('Name').text,
                              'rate': valute.find('Value').text,
-                             'currency_code':valute.find('CharCode').text,
-                             'nominal':valute.find('Nominal').text}
+                             'currency_code': valute.find('CharCode').text,
+                             'nominal': valute.find('Nominal').text}
             print(currency_data)
             return currency_data
     currency_data = {'name_of_currency': 'Указанная валюта не найдена',
                      'rate': '',
-                     'currency_code':''}
+                     'currency_code': ''}
     return currency_data
 
 
@@ -27,12 +27,15 @@ def get_currency(date_rate=datetime.now()):
     return root
 
 
-def all_currency(currency_list=[],date_rate=datetime.now()):
+def all_currency(currency_list=[], date_rate=datetime.now()):
     currencies_name = []
     root = get_currency(date_rate)
     for idx, valute in enumerate(root.iter('Valute')):
-        if not currency_list or valute.find('CharCode').text.upper() in currency_list:
-            valute_tuple = (valute.find('CharCode').text.upper(), valute.find('Name').text.upper())
+        if not currency_list or valute.find(
+                'CharCode').text.upper() in currency_list:
+            valute_tuple = (
+                valute.find('CharCode').text.upper(),
+                valute.find('Name').text.upper())
             currencies_name.append(valute_tuple)
 
     return currencies_name
@@ -41,20 +44,23 @@ def all_currency(currency_list=[],date_rate=datetime.now()):
 def rate_of_exchange(
         code_valute,
         date_rate=datetime.now()):
-    
+
     try:
         root = get_currency(date_rate)
         result = find_element(root, code_valute)
         return result
     except (requests.RequestException, ValueError):
-        
+
         return None
-    
-def notNone(rate,value):
+
+
+def notNone(rate, value):
     if rate is None:
         return value
     else:
         return rate
 
-def calculate_currency(value_rub,value_currency,nominal):
-   return round(float(notNone(value_rub,1))/float(value_currency.replace(',','.')),2)*float(nominal)
+
+def calculate_currency(value_rub, value_currency, nominal):
+    return round(float(notNone(value_rub, 1)) /
+                 float(value_currency.replace(',', '.')), 2) * float(nominal)
